d6d9ef293918ec269a9fd07ac02aa8c2053c8dac
Magomed_D <dzhamalovm05@gmail.com>
2025-10-29 00:27:48 +0300

    Add some little features for searching view

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
 urlpatterns = [
     path('', views.index, name="home"),
     path('post/<slug:slug>', views.post, name="post"),
