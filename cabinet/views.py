106303155ea3df966c79a0f4d5490b987617a701
Magomed_D <dzhamalovm05@gmail.com>
2025-10-30 22:35:57 +0300

    _

--- cabinet/views.py
+++ cabinet/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 @login_required(redirect_field_name="after", login_url="auth:login")
 def cabinet(request, user):
+    
+    if request.POST:
+        deleteAccount = request.POST["deleteAccount"]
+        if deleteAccount:
+            User.objects.get(username=user).delete()
+            messages.success(request, "Account successfully deleted!")
+            return redirect("auth:login")
+    else:
+        pass
+    
     data = {
     }
     return render(request, "cabinet/cabinet.html", context=data)
