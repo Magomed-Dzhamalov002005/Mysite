3286137c962c8dcd9d0a35042fefc76073e41951
Magomed_D <dzhamalovm05@gmail.com>
2025-11-01 14:25:08 +0300

    _

--- Mysite/settings.py
+++ Mysite/settings.py
@@ -93,15 +93,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
@@ -128,9 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static/'),
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- main/forms.py
+++ main/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Post, Comment
 
 class PostAddingForm(forms.ModelForm):
-    text = forms.CharField(widget=forms.TextInput)
+    text = forms.CharField(widget=forms.Textarea)
     
     class Meta:
         model = Post
